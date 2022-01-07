b45ea7bdaf689016939d8e82940c4eda02a66acb
Artem Kabilov <artem1223148@gmail.com>
2022-01-07 03:36:52 +0300

    Добавил кинозалы

--- cinema.cs
+++ cinema.cs
@@ -14,7 +14,31 @@ namespace MyVorm
         {
             this.Size = new System.Drawing.Size(500,500);
             this.BackColor = Color.LightGreen;
+            Button room_btn = new Button
+            {
+                Text = "saali valimine ja pileti ostmine",
+                Location = new System.Drawing.Point(200, 100),
+                Size = new Size(100,40)
+                
+            };
+            room_btn.BackColor = Color.White;
+            room_btn.Click += Room_btn_Click;
+            this.Controls.Add(room_btn);
+            Label lbl = new Label()
+            {
+                Text = "Tere tulemast kinno",
+                Location = new System.Drawing.Point(200, 30),
+                Size = new Size(100, 40)
+            };
+            this.Controls.Add(lbl);
+
         }
-           
+
+        private void Room_btn_Click(object sender, EventArgs e)
+        {
+            room room = new room("Cinema", "Valige saali suurus", "Väike", "Keskmine", "Suur");
+            room.StartPosition = FormStartPosition.CenterScreen;
+            room.ShowDialog();
+        }
     }
 }
